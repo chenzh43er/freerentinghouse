4bfefce2ff77f0295e509fe2e0e78a9589d4652f
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-07-14 03:23:39 +0800

    1

---
+++ Public/Js/googleAdv.js
@@ -0,0 +1,19 @@
+function returnTeach_adv1() {
+    return `<!-- /23284785848/teach/teach_adv1 -->
+<div id='div-gpt-ad-1751480638062-0'>
+  <script>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1751480638062-0'); });
+  </script>
+</div>`
+}
+
+function returnTestDiv(){
+    return `<!-- 广告容器 -->
+<div id="div-gpt-ad-123" style="width: 100%;"></div>`
+}
+
+function returnIndex_adv1(){
+    return `<!-- /23284785848/house_index/house_index1 -->
+<div id='div-gpt-ad-1752434042868-0' style='min-width: 300px; min-height: 250px;'>
+</div>`
+}
